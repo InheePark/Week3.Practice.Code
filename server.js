5b7e21600f4f1ce2f7ae66df86daaba438a01578
InheePark <59162676+InheePark@users.noreply.github.com>
2022-10-04 20:16:02 -0400

    week 2

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
-// the main entry point of node.js
-// this file will be used by users to invoke exported modules from 
+const connect = require('connect');
+// we are importing module to our entry point
+const app = connect();
+// connect to the server using connect module
 
-// in order to import from other modules to entry point, 
-// we use require('path'), then we will be able to use the modules
-// similar to import keyword in custom module
-const hello = require('./hello');
+function logger(req, res, next){
+    console.log(req.method, req.url);
 
-// hello.sayHello(); // we can use the exportable function
+    next();
+}
 
-// node js core modules
-// http server
-const http = require('http');
+function helloWorld(req, res, next){
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello World');
+}
 
-// creating a http server
-// req -> the request variable (passes on the request)
-// res -> the response of the server
-http.createServer((req, res) => {
-    // the res display code 
-    // we can specify how the return is displayed in the server
-    // 200 -> all is ok
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-        // content type of plain text
-    });
-    res.write('this is a response from the server!! \n\n');
-    // res.write -> writing (outputing) to the server
-    res.end('ending server');
-    // res.end -> ending the server
-}).listen(3000); // we specify the port number
+function goodByeWorld(req, res, next){
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Goodbye World');
+}
 
-console.log('Server running at http://localhost:3000/');
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodByeWorld);
+app.listen(3000);
+// connecting to the server port 3000
+console.log('Server is running at http://localhost:3000/');
